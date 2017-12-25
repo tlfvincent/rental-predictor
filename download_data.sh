0e05933b90011c742cee1a430702f3311acb45f5
Thomas Vincent <thomasvincent@Thomass-MBP.fios-router.home>
2017-12-25 18:12:51 -0500

    remove files before downloading

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+rm ./app/data/Neighborhood_MedianRentalPrice_Studio.csv
+rm ./app/data/Neighborhood_MedianRentalPrice_1Bedroom.csv
+rm ./app/data/Neighborhood_MedianRentalPrice_2Bedroom.csv
+rm ./app/data/Neighborhood_MedianRentalPrice_3Bedroom.csv
+rm ./app/data/Neighborhood_MedianRentalPrice_4Bedroom.csv
+
 wget http://files.zillowstatic.com/research/public/Neighborhood/Neighborhood_MedianRentalPrice_Studio.csv -O ./app/data/Neighborhood_MedianRentalPrice_Studio.csv
 wget http://files.zillowstatic.com/research/public/Neighborhood/Neighborhood_MedianRentalPrice_1Bedroom.csv -O ./app/data/Neighborhood_MedianRentalPrice_1Bedroom.csv
 wget http://files.zillowstatic.com/research/public/Neighborhood/Neighborhood_MedianRentalPrice_2Bedroom.csv -O ./app/data/Neighborhood_MedianRentalPrice_2Bedroom.csv
